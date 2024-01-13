5268d5f0c5b8f1106a7d1b232d9868d7829de4fc
Silpashree Acharya <silpashree@bootlabstech.com>
2024-01-13 12:29:25 +0530

    add res blcks

--- main.tf
+++ main.tf
@@ -5,7 +5,6 @@ resource "aws_lb" "balance_the_load" {
   ip_address_type    = var.ip_address_type
   security_groups    = var.security_groups
   subnets            = var.subnets
-
   enable_deletion_protection       = false
   desync_mitigation_mode           = "defensive"
   drop_invalid_header_fields       = false
@@ -19,6 +18,33 @@ resource "aws_lb" "balance_the_load" {
     ignore_changes = [tags,access_logs]
   }
   }
+  resource "aws_lb_target_group" "tg" {
+  name     = var.target_group_name
+  port     = var.target_port
+  protocol = var.target_protocol
+  vpc_id   = var.vpc_id
+  target_type = var.target_type
+}
+
+resource "aws_lb_target_group_attachment" "test" {
+  target_group_arn = aws_lb_target_group.tg.arn
+  target_id        = var.target_id
+  port             = var.target_port
+}
+
+resource "aws_lb_listener" "front_end" {
+  load_balancer_arn = aws_lb.balance_the_load.arn
+  port              = var.listener_port
+  protocol          = var.listener_protocol
+  ssl_policy        = var.ssl_policy
+  certificate_arn   = var.certificate_arn
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.tg.arn
+  }
+}
+
 
 
 
--- variables.tf
+++ variables.tf
@@ -19,12 +19,52 @@ variable "subnets" {
 variable "enable_deletion_protection" {
   type = bool
 }
-# variable "access_log_bucket" {
-#   type = string
-# }
 variable "ip_address_type" {
   type = string
 }
-# variable "access_log_enabled" {
-#   type = bool
-# }
+variable "target_group_name" {
+    description = "Target Group Name"
+    type = string  
+}
+
+variable "target_port" {
+  type = number
+  description = "target group port"
+}
+
+variable "target_protocol" {
+  type = string
+  description = "target group protocol eg. HTTP/S, TCP"
+}
+
+variable "vpc_id" {
+    description = "Shared VPC id"
+    type = string
+}
+
+variable "target_type" {
+    description = "Type of target group eg, instance,alb, lambda,ip "
+    type = string
+}
+
+variable "target_id" {
+  type = string
+  description = "instance id"
+}
+
+variable "listener_port" {
+  type = number
+  description = "listener port"
+}
+variable "ssl_policy" {
+  type = string
+  description = "ssl_policy"
+}
+variable "certificate_arn" {
+  type = string
+  description = "certificate_arn"
+}
+variable "listener_protocol" {
+  type = string
+  description = "Protocol for connections from clients to the load balancer"
+}
